5a3fb7765b523eddee38819f443ae8bac692aff6
Tsukasa9080-cyber <Tsukasa9080@gmail.com>
2026-08-14 10:08:16 +0900

    Initial Commit

---
+++ MatchingWithSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MatchingWithSwiftUI
+//
+//  Created by G-2028 on 2026/08/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MatchingWithSwiftUI/MatchingWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchingWithSwiftUIApp.swift
+//  MatchingWithSwiftUI
+//
+//  Created by G-2028 on 2026/08/14.
+//
+
+import SwiftUI
+
+@main
+struct MatchingWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
